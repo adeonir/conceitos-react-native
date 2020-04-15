14e57aea76f9a4e5eb7844c05fbbd131ec3d2622
Adeonir Kohl <adeonir@gmail.com>
2020-04-15 19:36:24 -0300

    Fix test, remove parentesis

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ export default function App() {
                   style={styles.likeText}
                   testID={`repository-likes-${repository.id}`}
                 >
-                  {repository.likes} curtida(s)
+                  {repository.likes} curtidas
                 </Text>
               </View>
 
